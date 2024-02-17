6430dc8fdc09190271cdbc7face7d25100f0bcd2
Linkon Miyan <rudlinkon@gmail.com>
2024-02-18 03:04:03 +0600

    initial commit

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install WP-CLI
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+# Install dist-archive command
+wp package install wp-cli/dist-archive-command
+
+# Generate WordPress Archive
+wp dist-archive . ./wordpress.zip
+
+# Upload Archive as GitHub Artifact
+echo "Uploading WordPress archive as artifact..."
+mkdir -p $HOME/artifacts
+mv ./wordpress.zip $HOME/artifacts/wordpress.zip
+echo "::set-output name=archive_path::$HOME/artifacts/wordpress.zip"
+
+# Set retention days for the uploaded artifact
+echo "Setting retention days for the uploaded artifact..."
+echo "::set-env name=ARTIFACT_RETENTION_DAYS::7"  # Adjust retention days as needed

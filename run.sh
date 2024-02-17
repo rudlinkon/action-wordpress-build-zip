941d61eb93cc970a00166a59548e0d06cfd16b56
Linkon Miyan <rudlinkon@gmail.com>
2024-02-18 05:48:26 +0600

    update shell status messages

--- run.sh
+++ run.sh
@@ -8,12 +8,12 @@ echo '✨Installing WP-CLI✨'
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
-echo '✅ Successfully install WP-CLI'
+echo '✅ Successfully installed WP-CLI'
 
 # Install dist-archive command
 echo '✨Installing dist-archive-command✨'
 wp package install wp-cli/dist-archive-command
-echo '✅ Successfully install dist-archive-command'
+echo '✅ Successfully installed dist-archive-command'
 
 # Generate WordPress Archive
 echo '✨Generating the archive file✨'

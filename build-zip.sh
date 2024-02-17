d36d977b43f0fe097a0e8a2d270f6910570f909b
Linkon Miyan <rudlinkon@gmail.com>
2024-02-18 04:41:30 +0600

    update messaged

--- build-zip.sh
+++ build-zip.sh
@@ -4,12 +4,18 @@
 set -eo
 
 # Install WP-CLI
+echo '✨Installing WP-CLI✨'
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
+echo '✅ Successfully install WP-CLI'
 
 # Install dist-archive command
+echo '✨Installing dist-archive-command✨'
 wp package install wp-cli/dist-archive-command
+echo '✅ Successfully install dist-archive-command'
 
 # Generate WordPress Archive
+echo '✨Generating the archive file✨'
 wp dist-archive . ./wordpress.zip
+echo '🎉 Successfully generated archive file'
